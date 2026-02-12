7a4f384ffa7bee0362c30e345aa1b1b193b377ac
Alirzeanoroozi <noroozi.alirezaa@gmail.com>
2026-02-12 10:45:06 +0300

    remove the cif files

--- run_files/pdb_download.py
+++ run_files/pdb_download.py
@@ -1,26 +1,10 @@
 import os
-import subprocess
 import gzip
 import shutil
 import urllib.request
 
 TARGET_DIR = "processed/pdbs"
 os.makedirs(TARGET_DIR, exist_ok=True)
-
-def convert_mmcif_to_pdb(mmcif_filepath, pdb_dir):
-    try:
-        beem_exe = "external_tools/BeEM-master/BeEM"
-
-        ret = subprocess.call([beem_exe, mmcif_filepath], cwd=pdb_dir)
-        if ret != 0:
-            raise RuntimeError("BeEM failed")
-
-        merge_script = "run_files/merge_bundles.py"
-        subprocess.call(["python", merge_script, "*-bundle*.pdb", pdb_dir])
-        return True
-    except Exception as e:
-        print(f"Error converting mmCIF to PDB: {e}")
-        return False
 
 def download_pdb_file(pdb_name, pdb_dir):
     final_pdb = f"{pdb_dir}/{pdb_name}.pdb"
@@ -41,26 +25,6 @@ def download_pdb_file(pdb_name, pdb_dir):
         return True
     except Exception as e:
         print(f"PDB download failed for {pdb_name}: {e}")
-
-    try:
-        mmcif_gz = f"{pdb_dir}/{pdb_name}.cif.gz"
-        mmcif_file = f"{pdb_dir}/{pdb_name}.cif"
-
-        mmcif_url = f"https://files.pdbj.org/pub/pdb/data/structures/all/mmCIF/{pdb_name[:4].lower()}.cif.gz"
-        response = urllib.request.urlopen(mmcif_url)
-
-        with open(mmcif_gz, "wb") as fh:
-            fh.write(response.read())
-
-        with gzip.open(mmcif_gz, "rb") as f_in, open(mmcif_file, "wb") as f_out:
-            shutil.copyfileobj(f_in, f_out)
-
-        os.remove(mmcif_gz)
-
-        return convert_mmcif_to_pdb(mmcif_file, final_pdb)
-
-    except Exception as e:
-        print(f"Error downloading {pdb_name}: {e}")
         return False
 
 def pdb_downloader():
